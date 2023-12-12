1f233a6d2f89672bfa5771d98c9d3997f4999e11
Ilya Dmitrichenko <errordeveloper@gmail.com>
2023-12-12 14:56:34 +0000

    Add essential functionality
    - handle nil config
    - set node image
    - client config and controller-manager constructors

--- provider/kind/provider.go
+++ provider/kind/provider.go
@@ -6,7 +6,10 @@ import (
 
 	"github.com/google/uuid"
 
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	klog "k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	"sigs.k8s.io/kind/pkg/cluster"
 
@@ -24,17 +27,19 @@ type Kind struct {
 	UUID uuid.UUID
 	*cluster.Provider
 
+	NodeImage   string
 	ArtifactDir string
 	Logger      klog.Logger
 }
 
 func New(artifactDir string, logger klog.Logger) *Kind {
-	logAdapter := &log.Adapter{logger.WithName("kind")}
 
+	logAdapter := &log.Adapter{logger.WithName("kind")}
+	uuid := uuid.New()
 	return &Kind{
-		UUID:        uuid.New(),
+		UUID:        uuid,
 		ArtifactDir: artifactDir,
-		Logger:      logger.WithName("kind-provider"),
+		Logger:      logger.WithName("kind-provider").WithValues("kind-provider-uuid", uuid.String()),
 		Provider:    cluster.NewProvider(cluster.ProviderWithLogger(logAdapter)),
 	}
 }
@@ -52,18 +57,48 @@ func (k *Kind) LogsDir() string {
 }
 
 func (k *Kind) Create(config *configv1alpha4.Cluster, timeout time.Duration) error {
-	return k.Provider.Create(
-		k.ClusterName(),
-		//cluster.CreateWithV1Alpha4Config(config),
+	options := []cluster.CreateOption{
 		cluster.CreateWithKubeconfigPath(k.KubeConfigPath()),
 		cluster.CreateWithDisplayUsage(false),
 		cluster.CreateWithDisplaySalutation(false),
 		cluster.CreateWithWaitForReady(timeout),
-	)
+	}
+	if config != nil {
+		options = append(options, cluster.CreateWithV1Alpha4Config(config))
+	}
+	if k.NodeImage != "" {
+		options = append(options, cluster.CreateWithNodeImage(k.NodeImage))
+	}
+	return k.Provider.Create(k.ClusterName(), options...)
 }
 
 func (k *Kind) CollectLogs() error {
 	return k.Provider.CollectLogs(k.ClusterName(), k.LogsDir())
+}
+
+func (k *Kind) NewControllerManager() (ctrl.Manager, error) {
+	clientConfig, err := k.NewClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	logger := k.Logger.WithValues("kind-provider-uuid", k.UUID.String())
+	ctrl.SetLogger(logger.WithName("kte-controller-runtime"))
+	options := ctrl.Options{
+		Logger: logger.WithName("kte-controller-manager"),
+	}
+	return ctrl.NewManager(clientConfig, options)
+}
+
+func (k *Kind) NewClientConfig() (*rest.Config, error) {
+	loader := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{
+			ExplicitPath: k.KubeConfigPath(),
+		},
+		&clientcmd.ConfigOverrides{
+			CurrentContext: Name + "-" + k.ClusterName(),
+		})
+
+	return loader.ClientConfig()
 }
 
 func (k *Kind) Delete() error {
--- provider/kind/provider_test.go
+++ provider/kind/provider_test.go
@@ -8,20 +8,23 @@ import (
 
 	. "github.com/onsi/gomega"
 
+	corev1 "k8s.io/api/core/v1"
 	klog "k8s.io/klog/v2"
-
-	configv1alpha4 "sigs.k8s.io/kind/pkg/apis/config/v1alpha4"
 
 	"github.com/errordeveloper/kube-test-env/provider/kind"
 )
 
-func TestKind(t *testing.T) {
+func TestKindCreateAccessDelete(t *testing.T) {
 	g := NewWithT(t)
 
 	log := klog.FromContext(context.Background())
 	k := kind.New(t.TempDir(), log)
 
-	g.Expect(k.Create(&configv1alpha4.Cluster{}, time.Minute*10)).To(Succeed())
+	g.Expect(k.Create(nil, time.Minute*10)).To(Succeed())
+
+	clusters, err := k.Provider.List()
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(clusters).To(ContainElement(k.ClusterName()))
 
 	t.Logf("Created cluster name=%q kubeconfig=%q", k.ClusterName(), k.KubeConfigPath())
 
@@ -51,8 +54,47 @@ func TestKind(t *testing.T) {
 	g.Expect(cpLogs("pods")).To(BeADirectory())
 	g.Expect(cpLogs("containers")).To(BeADirectory())
 
+	clientConfig, err := k.NewClientConfig()
+	g.Expect(err).NotTo(HaveOccurred())
+
+	g.Expect(clientConfig).NotTo(BeNil())
+	g.Expect(clientConfig.Host).To(HavePrefix("https://127.0.0.1:"))
+	g.Expect(clientConfig.Username).To(BeEmpty())
+	g.Expect(clientConfig.Password).To(BeEmpty())
+	g.Expect(clientConfig.BearerToken).To(BeEmpty())
+	g.Expect(clientConfig.BearerTokenFile).To(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig).NotTo(BeNil())
+	g.Expect(clientConfig.TLSClientConfig.Insecure).To(BeFalse())
+	g.Expect(clientConfig.TLSClientConfig.ServerName).To(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.CertFile).To(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.KeyFile).To(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.CAFile).To(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.CertData).ToNot(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.KeyData).ToNot(BeEmpty())
+	g.Expect(clientConfig.TLSClientConfig.CAData).ToNot(BeEmpty())
+
+	mgr, err := k.NewControllerManager()
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(mgr).NotTo(BeNil())
+
+	nodes := &corev1.NodeList{}
+	ctx := context.Background()
+	go func() {
+		err := mgr.Start(ctx)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	g.Expect(mgr.GetClient().List(ctx, nodes)).To(Succeed())
+	g.Expect(nodes.Items).To(HaveLen(1))
+
 	g.Expect(k.Delete()).To(Succeed())
 
-	t.Logf("Deleted cluster name=%q", k.ClusterName())
+	clusters, err = k.Provider.List()
+	g.Expect(err).NotTo(HaveOccurred())
 
+	g.Expect(clusters).ToNot(ContainElement(k.ClusterName()))
+
+	t.Logf("Deleted cluster name=%q", k.ClusterName())
 }
